8756518322756115da8bbc08ea5318f2f573a4a0
Mike Oram <mike.oram@mayden.co.uk>
2022-12-15 13:53:46 +0000

    create HTML structure

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Social Content Exercise</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+</head>
+<body>
+
+<main class="container">
+    <div class="row">
+        <div class="col-12">
+            <h1>Social content exercise</h1>
+            <p>Replace the below content with the data from the supplied DB.</p>
+        </div>
+        <div class="col-12">
+
+            <div class="card mb-4">
+                <div class="card-body">
+                    <h5 class="card-title">bittersweet <small class="small mr-2 float-right text-muted">14/12/2022 14:07:15</small></h5>
+                    <p class="card-text">Just took the most gorgeous sunset walk - what a way to end the day!</p>
+                    <div class="btn-group" role="group">
+                        <button type="button" class="btn btn-info">-</button>
+                        <span class="disabled btn btn-outline-info">0</span>
+                        <button type="button" class="btn btn-info">+</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card mb-4">
+                <div class="card-body">
+                    <h5 class="card-title">gregorygreg <small class="small mr-2 float-right text-muted">14/12/2022 14:05:52</small></h5>
+                    <p class="card-text">Can't believe it's already October - this year is flying by...wait it's December?!</p>
+                    <div class="btn-group" role="group">
+                        <button type="button" class="btn btn-info">-</button>
+                        <span class="disabled btn btn-outline-info">1</span>
+                        <button type="button" class="btn btn-info">+</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card mb-4">
+                <div class="card-body">
+                    <h5 class="card-title">flowerpower <small class="small mr-2 float-right text-muted">14/12/2022 14:05:09</small></h5>
+                    <p class="card-text">Picked up some fresh flowers for the house - so nice to have a little bit of nature indoors!</p>
+                    <div class="btn-group" role="group">
+                        <button type="button" class="btn btn-info">-</button>
+                        <span class="disabled btn btn-outline-info">4</span>
+                        <button type="button" class="btn btn-info">+</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card mb-4">
+                <div class="card-body">
+                    <h5 class="card-title">iamsarah <small class="small mr-2 float-right text-muted">14/12/2022 14:04:50</small></h5>
+                    <p class="card-text">Brunch with friends is the best way to start the weekend</p>
+                    <div class="btn-group" role="group">
+                        <button type="button" class="btn btn-info">-</button>
+                        <span class="disabled btn btn-outline-info">5</span>
+                        <button type="button" class="btn btn-info">+</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card mb-4">
+                <div class="card-body">
+                    <h5 class="card-title">amyrobson <small class="small mr-2 float-right text-muted">14/12/2022 14:04:14</small></h5>
+                    <p class="card-text">Just finished up a great workout at the gym and feeling so energized!</p>
+                    <div class="btn-group" role="group">
+                        <button type="button" class="btn btn-info">-</button>
+                        <span class="disabled btn btn-outline-info">12</span>
+                        <button type="button" class="btn btn-info">+</button>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</main>
+
+</body>
+</html>
